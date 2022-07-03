1dcbff899a0609cb9196f2cf01780777b6d2de1c
Li Zhineng <im@zhineng.li>
2022-07-03 10:25:34 +0800

    block & pill

--- lib/components/button/button.ts
+++ lib/components/button/button.ts
@@ -4,9 +4,33 @@ Component({
       type: String,
       value: 'secondary'
     },
+    pill: {
+      type: Boolean,
+      value: false
+    },
+    block: {
+      type: Boolean,
+      value: false
+    },
     disabled: {
       type: Boolean,
       value: false
+    }
+  },
+
+  data: {
+    classes: 'btn'
+  },
+
+  observers: {
+    'pill, block'(pill, block) {
+      this.setData({
+        classes: [
+          'btn',
+          pill ? 'btn-pill' : null,
+          block ? 'btn-block' : null
+        ].filter(x => x)
+      })
     }
   }
 })
