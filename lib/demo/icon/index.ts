53541ac8efa8b6d719ff1bbaa0aa787d9f8d6de1
Li Zhineng <im@zhineng.li>
2022-07-05 22:34:57 +0800

    icons

---
+++ lib/demo/icon/index.ts
@@ -0,0 +1,73 @@
+Page({
+  data: {
+    icons: [
+      {
+        label: 'Navigation',
+        category: 'navigation',
+        icons: [
+          'arrow-up',
+          'arrow-right',
+          'arrow-down',
+          'arrow-left',
+          'three-dots-horizontal',
+          'three-dots-vertical',
+          'plus',
+          'minus',
+          'close',
+          'double-arrow-right',
+          'help-circle',
+          'info-circle',
+          'filter',
+          'share',
+          'plus-circle',
+          'gears'
+        ]
+      },
+
+      {
+        label: 'User',
+        category: 'user',
+        icons: [
+          'user',
+          'user-circle',
+          'user-plus',
+          'user-level-up'
+        ]
+      },
+
+      {
+        label: 'File',
+        category: 'file',
+        icons: [
+          'download',
+          'upload',
+          'upload-to-cloud',
+          'download-from-cloud',
+          'trash-bin',
+          'magnifying',
+          'paper',
+          'clipboard'
+        ]
+      },
+
+      {
+        label: 'Finance',
+        category: 'finance',
+        icons: [
+          'trending-up',
+          'trending-down',
+          'currency-yen'
+        ]
+      },
+
+      {
+        label: 'Geography',
+        category: 'geography',
+        icons: [
+          'globe',
+          'location'
+        ]
+      }
+    ]
+  }
+})
